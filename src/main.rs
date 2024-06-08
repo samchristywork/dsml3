339232f7eb1af74221d828d5adbe6913992e8e02
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-07 21:21:51 -0500

    feat: Add function for drawing text boxes
    This function handles some text wrapping so that we can draw multiline
    text boxes. The text boxes have a border around them.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,45 @@ struct Args {
     output: String,
 }
 
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+fn draw_text_box(
+    cr: &Context,
+    pos: &Vec2,
+    width: f64,
+    text: &str,
+) {
+    let mut height = 0;
+    let mut line = String::new();
+    for word in text.split_whitespace() {
+        let new_line = match line.len() {
+            0 => word.to_string(),
+            _ => line.clone() + " " + word,
+        };
+
+        let extents = cr.text_extents(new_line.as_str()).unwrap();
+        if extents.width() > width {
+            cr.move_to(pos.x, pos.y + 12.0 + height);
+            cr.show_text(line.as_str()).unwrap();
+            height += 12.0;
+            line = word.to_string();
+        } else {
+            line = new_line;
+        }
+    }
+
+    cr.move_to(pos.x, pos.y + 12.0 + height);
+    cr.show_text(line.as_str()).unwrap();
+
+    height += 12.0;
+
+    cr.rectangle(pos.x, pos.y, width, height);
+    cr.stroke().unwrap();
+}
+
 fn main() {
     let args = Args::parse();
 }
